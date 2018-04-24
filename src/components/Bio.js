a1a0b0b68bdc19f6b1559124ed8377132e5fd230
Kalle Ott <kalle.ott@cap3.de>
2018-04-24 10:51:33 +0200

    use outboundlink on bio page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import glamorous from 'glamorous'
+import React from "react";
+import glamorous from "glamorous";
 // Import typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
+import "typeface-montserrat";
+import "typeface-merriweather";
 
-import profilePic from './profile-kao.jpeg'
-import { rhythm } from '../utils/typography'
+import profilePic from "./profile-kao.jpeg";
+import { rhythm } from "../utils/typography";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const Container = glamorous.div({
-  display: 'flex',
+  display: "flex",
   marginBottom: rhythm(2.5),
-})
+});
 
 const Avatar = glamorous.div(
   {
@@ -18,18 +19,18 @@ const Avatar = glamorous.div(
     marginBottom: 0,
     width: rhythm(2),
     height: rhythm(2),
-    borderRadius: '100%',
-    backgroundPosition: '50%',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
+    borderRadius: "100%",
+    backgroundPosition: "50%",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
     flexShrink: 0,
-    textDecoration: 'none',
-    color: 'inherit',
+    textDecoration: "none",
+    color: "inherit",
   },
   ({ image }) => ({
     backgroundImage: `url(${image})`,
-  })
-)
+  }),
+);
 
 class Bio extends React.Component {
   render() {
@@ -37,15 +38,15 @@ class Bio extends React.Component {
       <Container>
         <Avatar image={profilePic} alt={`Kalle Ott`} />
         <p>
-          Written by{' '}
-          <a href="https://twitter.com/kaoDev">
-            <strong>Kalle Ott</strong>
-          </a>{' '}
+          Written by{" "}
+          <OutboundLink href="https://twitter.com/kaoDev">
+            <strong>Kalle Ott</strong>{" "}
+          </OutboundLink>
           who lives and works in Kiel
         </p>
       </Container>
-    )
+    );
   }
 }
 
-export default Bio
+export default Bio;
